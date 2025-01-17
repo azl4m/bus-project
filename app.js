078be607097eefe62d73dd8d6cd116752878c08d
Nabeel Aslam <nabeel.azlam7@gmail.com>
2025-01-17 15:33:25 +0530

    fixed partials

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({extended:true}))
 
 app.set("view engine","ejs")
 app.set("views",[path.join(__dirname,"views/user"),path.join(__dirname,"views/admin")])
+app.use(express.static(path.join(__dirname,"public")))
 
 const port = process.env.PORT
 app.listen(port,()=> console.log("server running "+port))
