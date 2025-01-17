22125cb05e888f1242e5629dab11c4e150300376
Nabeel Aslam <nabeel.azlam7@gmail.com>
2025-01-17 16:36:33 +0530

    added taxi page and footer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const busController = require('../controllers/userControllers/busController')
+const taxiController = require("../controllers/userControllers/taxiController")
 
 const router = express.Router()
 
 router.get('/',busController.busSearch)
+router.get("/auto-taxi",taxiController.getTaxiPage)
 
 module.exports = router
