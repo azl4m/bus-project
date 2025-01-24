9fa8068591a22f7d79cf9cea2cbf6c71ad7ca02f
Nabeel Aslam <nabeel.azlam7@gmail.com>
2025-01-24 20:39:23 +0530

    fetch aded

--- controllers/adminController/placeController.js
+++ controllers/adminController/placeController.js
@@ -1,27 +1,39 @@
 const Bus = require('../../models/Bus')
 const Place = require('../../models/Place')
 const Route = require('../../models/Route')
-
+const placeHelper = require('../../helpers/placeHelper')
 const getAddPlace = async(req,res)=>{
     try {
-        res.render('add-place',{success:false})
+        res.render('add-place')
     } catch (error) {
         console.log(error);
     }
 }
 const postAddPlace = async(req,res) => {
     try {
-        const { place, city, district } = req.body
+        let { place, city, district } = req.body
+        place = placeHelper.capitalizeFirstLetter(place)
+        city = placeHelper.capitalizeFirstLetter(city)
+        district = placeHelper.capitalizeFirstLetter(district)
+        const existingPlace = Place.findOne({
+            $and: [
+                { place: place },
+                { city: city }
+              ]
+        })
+        if(existingPlace){
+            return res.status(400).json({message:"Place and City combination already exists"})
+        }
         const newPlace = new Place({
             place,
             city,
             district
         })
         await newPlace.save()
-        res.render('add-place',{success:true})
+        res.status(200).json({message:"Place added succesfully"})
     } catch (error) {
         console.log(error);
-        
+       res.status(500).json({message:"Internal Server Error"}) 
     }
 }
 
