3a1f72d7f1dc10cac8b1487a5451655cb49bfda4
Hakeem Muhammed <hakeemnk18@gmail.com>
2025-02-26 17:38:06 +0530

    taxi listed

--- models/taxi.js
+++ models/taxi.js
@@ -4,6 +4,7 @@ const taxiSchema = new mongoose.Schema({
   name: { type: String, required: true }, 
   type: { type: String, enum: ["car", "goods", "traveler","auto","ambulance","for-wheeler-goods","lory"], required: true },
   phone: { type: String, required: true, unique: true },
+  isActive : { type : Boolean, default : true},
   placeId:{type: mongoose.Schema.Types.ObjectId,ref: 'Place',required: true}
   
 },{timestamps:true});
